1822f832b2d5efafe22689d9b9cc5e64f0a8b21c
Anas Tanwar <anastanwar5@gmail.com>
2024-11-17 07:16:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //for newer version
+
+console.log("Anas");
+
+
+let age=18
+let isLoggedIn= false
+let state =null
+
+/*
+number=> 2 to the power 53
+bigInt
+string=""
+boolean=true/false
+null=standalone
+undefined=> not defined
+symbol=>unique
+
+    object=>
+
+*/
+
+console.log(typeof "null ")
