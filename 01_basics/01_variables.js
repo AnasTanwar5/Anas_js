7815fe24b25b10607869cc1b4a180987efccd771
Anas Tanwar <anastanwar5@gmail.com>
2024-11-17 07:16:45 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123455
+let accountMail ="anas@gmail.com"
+var accountPassword="anas"
+accountCity = "Jaipur"
+
+// accountId =2  //not allowed
+/*
+do not use var 
+*/
+accountMail="haudhi"
+accountPassword="abaj"
+accountCity="mumbai"
+console.log(accountId);
+console.table([accountId,accountMail,accountPassword,accountCity])
